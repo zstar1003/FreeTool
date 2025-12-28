f85fa5a3fd129cde584bdbd076254ef5ad80a3a0
zstar1003 <1198768105@qq.com>
2025-12-28 13:08:30 +0800

    build: 调整vite路径以适配esa

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import viteCompression from 'vite-plugin-compression';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: mode === 'production' ? '/FreeTool/' : '/',
+      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
